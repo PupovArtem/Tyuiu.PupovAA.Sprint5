0b891fb34be32fd57c66951c8b8a14ca20f656ee
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-11-25 23:34:40 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.PupovAA.Sprint5.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.PupovAA.Sprint5.Task0.V9.Lib
+{
+    public class DataService : ISprint5Task0V9
+    {
+        public string SaveToFileTextData(int x)
+        {
+            
+            double y = Math.Pow(x, 3) / (Math.Pow(x, 2) - 1);
+            y = Math.Round(y, 3);
+            string fileName = "OutPutFileTask0.txt";
+            string filePath = Path.Combine(Path.GetTempPath(), fileName);
+            File.WriteAllText(filePath, Convert.ToString(y));
+            return filePath;
+
+
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint5.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PupovAA.Sprint5.Task0.V9.Lib;
+namespace Tyuiu.PupovAA.Sprint5.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\artem\AppData\Local\Temp\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fx= fileInfo.Exists;
+            Assert.AreEqual(true, fx);
+        }
+    }
+}
